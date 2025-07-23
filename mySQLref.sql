31f403a1425ef5904feceb962f77393fab87956b
charles clarke <charles_b_clarke@outlook.com>
2025-07-23 13:00:08 -0700

    adjusting for local use

--- mySQLref.sql
+++ mySQLref.sql
@@ -1,30 +1,127 @@
-kCREATE TABLE IF NOT EXISTS objects (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL
+CREATETABLEIFNOTEXISTSobjects(
+    idINTAUTO_INCREMENTPRIMARYKEY,
+    serialVARCHAR(64)NOTNULLUNIQUE,
+    nameVARCHAR(255)NOTNULL
 );
-
-CREATE TABLE IF NOT EXISTS relationships (
-    parent_id INT,
-    child_id INT,
-    PRIMARY KEY (parent_id, child_id),
-    FOREIGN KEY (parent_id) REFERENCES objects(id) ON DELETE CASCADE,
-    FOREIGN KEY (child_id) REFERENCES objects(id) ON DELETE CASCADE
+CREATETABLEIFNOTEXISTSrelationships(
+parent_idINT,
+child_idINT,
+PRIMARYKEY(
+    parent_id,
+    child_id
+),
+FOREIGNKEY(
+    parent_id
+)REFERENCESobjects(id)
+    ON DELETECASCADEFOREIGNKEY(
+        child_id
+    )REFERENCESobjects(id)
+        ON DELETECASCADE
 );
-
 -- Add root object
-INSERT INTO objects (serial, name) VALUES ('ROOT001', 'Root Item');
-
+INSERT INTO objects(
+serial,
+name
+)
+VALUES(
+'ROOT001',
+'Root Item'
+);
 -- Insert new objects
-INSERT INTO objects (serial, name) VALUES 
-    ('ITEM002', 'Item A'),
-    ('ITEM003', 'Item B'),
-    ('ITEM004', 'Item C'),
-    ('ITEM005', 'Item D');
-
+INSERT INTO objects(
+serial,
+name
+)
+VALUES(
+'ITEM002',
+'Item A'
+),
+(
+'ITEM003',
+'Item B'
+),
+(
+'ITEM004',
+'Item C'
+),
+(
+'ITEM005',
+'Item D'
+);
 -- Establish parent-child relationships
-INSERT INTO relationships (parent_id, child_id) VALUES 
-    ((SELECT id FROM objects WHERE serial = 'ROOT001'), (SELECT id FROM objects WHERE serial = 'ITEM002')),
-    ((SELECT id FROM objects WHERE serial = 'ROOT001'), (SELECT id FROM objects WHERE serial = 'ITEM003')),
-    ((SELECT id FROM objects WHERE serial = 'ITEM002'), (SELECT id FROM objects WHERE serial = 'ITEM004')),
-    ((SELECT id FROM objects WHERE serial = 'ITEM003'), (SELECT id FROM objects WHERE serial = 'ITEM005'));
-
+INSERT INTO relationships(
+parent_id,
+child_id
+)
+VALUES(
+(
+SELECT
+id
+FROM
+objects
+WHERE
+serial = 'ROOT001'
+),
+(
+SELECT
+id
+FROM
+objects
+WHERE
+serial = 'ITEM002'
+)
+),
+(
+(
+SELECT
+id
+FROM
+objects
+WHERE
+serial = 'ROOT001'
+),
+(
+SELECT
+id
+FROM
+objects
+WHERE
+serial = 'ITEM003'
+)
+),
+(
+(
+SELECT
+id
+FROM
+objects
+WHERE
+serial = 'ITEM002'
+),
+(
+SELECT
+id
+FROM
+objects
+WHERE
+serial = 'ITEM004'
+)
+),
+(
+(
+SELECT
+id
+FROM
+objects
+WHERE
+serial = 'ITEM003'
+),
+(
+SELECT
+id
+FROM
+objects
+WHERE
+serial = 'ITEM005'
+)
+);
